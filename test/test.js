58ac17dfbf6cda2b077d3006d4ed5205dade48f6
ben <beneaves01@hotmail.com>
2019-05-15 14:42:50 +0800

    init

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+
+
 const ecdsa = {
   u82str: function(STR) {
     let str = ''
@@ -131,3 +133,21 @@ const ecdsa = {
     }
   }
 }
+
+//test
+
+ecdsa.gen('521', function(err, gen){
+  if(err){return console.log(err)}
+  //console.log(gen)
+  ecdsa.sign(gen.private, 'test', '512', 'u8', function(err, res){
+    if(err){return console.log(err)}
+    console.log(res)
+    ecdsa.verify(gen.public, res, 'test', '512', 'u8', function(err, res){
+      if(err){return console.log(err)}
+      if(res){
+        return console.log('ecdsa test pass')
+      }
+      return console.log('ecdsa test fail')
+    })
+  })
+})
